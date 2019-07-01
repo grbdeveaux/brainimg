d50645dd51672ae1267ccb83ac709415799a5bcc
Gabriel Deveaux <grbdeveaux@gmail.com>
2019-07-01 16:34:10 -0230

    Blending
    Now blends segments into original image. Still work to be done to ignore non-tumors.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # https://stackoverflow.com/questions/49834264/mri-brain-tumor-image-processing-and-segmentation-skull-removing
 
 import numpy as np
-import cv2
+import cv2 as cv
 import scipy.io
 
 from matplotlib import pyplot as plt
@@ -11,11 +11,11 @@ from skimage.morphology import watershed as skwater
 def ShowImage(title,img,ctype):
     plt.figure(figsize=(10, 10))
     if ctype=='bgr':
-        b,g,r = cv2.split(img)       # get b,g,r
-        rgb_img = cv2.merge([r,g,b])     # switch it to rgb
+        b,g,r = cv.split(img)       # get b,g,r
+        rgb_img = cv.merge([r,g,b])     # switch it to rgb
         plt.imshow(rgb_img)
     elif ctype=='hsv':
-        rgb = cv2.cvtColor(img,cv2.COLOR_HSV2RGB)
+        rgb = cv.cvtColor(img,cv.COLOR_HSV2RGB)
         plt.imshow(rgb)
     elif ctype=='gray':
         plt.imshow(img,cmap='gray')
@@ -28,8 +28,8 @@ def ShowImage(title,img,ctype):
     plt.show()
 
 # Read in image
-img = cv2.imread('./scans/brain1.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv.imread('./scans/brain1.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 #ShowImage('Brain with Skull', gray, 'gray')
 
 #Make a histogram of the intensities in the grayscale image
@@ -37,15 +37,15 @@ plt.hist(gray.ravel(),256)
 #plt.show()
 
 #Threshold the image to binary using Otsu's method
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
+ret, thresh = cv.threshold(gray,0,255,cv.THRESH_OTSU)
 #ShowImage('Applying Otsu',thresh,'gray')
 
 colormask = np.zeros(img.shape, dtype=np.uint8)
 colormask[thresh!=0] = np.array((0,0,255))
-blended = cv2.addWeighted(img,0.7,colormask,0.1,0)
+blended = cv.addWeighted(img,0.7,colormask,0.1,0)
 #ShowImage('Blended', blended, 'bgr')
 
-ret, markers = cv2.connectedComponents(thresh)
+ret, markers = cv.connectedComponents(thresh)
 
 #Get the area taken by each component. Ignore label 0 since this is the background.
 marker_area = [np.sum(markers==m) for m in range(np.max(markers)) if m!=0]
@@ -56,7 +56,7 @@ brain_mask = markers==largest_component
 
 brain_mask = np.uint8(brain_mask)
 kernel = np.ones((8,8),np.uint8)
-closing = cv2.morphologyEx(brain_mask, cv2.MORPH_CLOSE, kernel)
+closing = cv.morphologyEx(brain_mask, cv.MORPH_CLOSE, kernel)
 #ShowImage('Closing', closing, 'gray')
 
 brain_out = img.copy()
@@ -76,23 +76,71 @@ brain_out[closing==False] = (0,0,0)
 # Watershed isn't working super well... I think thresholding and only keeping higher values would help.
 # The separations here are not obvious. We need to really remove anything darker than 200
 
-gray = cv2.cvtColor(brain_out,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+gray = cv.cvtColor(brain_out,cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
 
-equ = cv2.equalizeHist(gray)
+equ = cv.equalizeHist(gray)
 ret = np.hstack((gray,equ)) #stacking images side-by-side
-cv2.imwrite('res.png',ret)
+cv.imwrite('res.png',ret)
 
 # https://docs.opencv.org/3.4.3/d7/d4d/tutorial_py_thresholding.html
 
-ret,img = cv2.threshold(img,160,255,cv2.THRESH_BINARY)
+ret,img = cv.threshold(img,150,255,cv.THRESH_BINARY)
 
-cv2.imwrite('Pre Erosion.png',img)
+cv.imwrite('Pre Erosion.png',img)
 
 kernel = np.ones((5,5),np.uint8)
-erosion = cv2.erode(img,kernel,iterations = 1)
 
-cv2.imwrite('Post Erosion.png',erosion)
+#img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+#cv.imwrite('Post Opening.png',img)
+img = cv.erode(img,kernel,iterations = 2)
+cv.imwrite('Post Erosion1.png',img)
+img = cv.dilate(img,kernel,iterations = 2)
+cv.imwrite('Post Dilation1.png',img)
+
+
+#https://answers.opencv.org/question/97416/replace-a-range-of-colors-with-a-specific-color-in-python/
+image = np.zeros((400,400,3), dtype="uint8")
+image[np.where((image==[0,0,0]).all(axis=2))] = [255,255,255]
+
+
+# https://docs.opencv.org/3.4/d5/dc4/tutorial_adding_images.html
+
+alpha = 0.5
+try:
+    raw_input          # Python 2
+except NameError:
+    raw_input = input  # Python 3
+
+
+print(''' Simple Linear Blender
+-----------------------
+* Enter alpha [0.0-1.0]: ''')
+input_alpha = float(raw_input().strip())
+if 0 <= alpha <= 1:
+    alpha = input_alpha
+# [load]
+src1 = cv.imread(cv.samples.findFile('./scans/brain1.png'))
+src2 = img
+# [load]
+if src1 is None:
+    print("Error loading src1")
+    exit(-1)
+elif src2 is None:
+    print("Error loading src2")
+    exit(-1)
+# [blend_images]
+beta = (1.0 - alpha)
+dst = cv.addWeighted(src1, alpha, src2, beta, 0.0)
+cv.imwrite('Blendaroony.png',dst)
+# [blend_images]
+# [display]
+cv.imshow('dst', dst)
+cv.waitKey(0)
+# [display]
+cv.destroyAllWindows()
+
+
 
 # How do we remove the bright spots that aren't tumours?
 # Maybe this is just a bad image and the real images won't have any big bright spots other than the tumour.
@@ -107,22 +155,22 @@ cv2.imwrite('Post Erosion.png',erosion)
 
 # noise removal
 kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations = 2)
 # sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
+sure_bg = cv.dilate(opening,kernel,iterations=3)
 # Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
+unknown = cv.subtract(sure_bg,sure_fg)
 
 # Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
+ret, markers = cv.connectedComponents(sure_fg)
 # Add one to all labels so that sure background is not 0, but 1
 markers = markers+1
 # Now, mark the region of unknown with zero
 markers[unknown==255] = 0
 
-markers = cv2.watershed(img,markers)
+markers = cv.watershed(img,markers)
 img[markers == -1] = [255,0,0]
