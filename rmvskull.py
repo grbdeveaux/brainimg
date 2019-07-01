2394e7ff5ffc404811df2472a0f1cb9f3dbc6c74
Gabriel Deveaux <grbdeveaux@gmail.com>
2019-07-01 00:44:33 -0230

    Up to skull being removed...

--- rmvskull.py
+++ rmvskull.py
@@ -34,7 +34,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #Make a histogram of the intensities in the grayscale image
 plt.hist(gray.ravel(),256)
-#plt.show()
+plt.show()
 
 #Threshold the image to binary using Otsu's method
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
@@ -63,57 +63,3 @@ brain_out = img.copy()
 #In a copy of the original image, clear those pixels that don't correspond to the brain
 brain_out[closing==False] = (0,0,0)
 #ShowImage('Connected Components222',brain_out,'rgb')
-
-
-
-
-
-
-
-
-
-# https://docs.opencv.org/3.3.1/d3/db4/tutorial_py_watershed.html
-# Watershed isn't working super well... I think thresholding and only keeping higher values would help.
-# The separations here are not obvious. We need to really remove anything darker than 200
-
-gray = cv2.cvtColor(brain_out,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-equ = cv2.equalizeHist(gray)
-ret = np.hstack((gray,equ)) #stacking images side-by-side
-cv2.imwrite('res.png',ret)
-
-
-
-# How do we remove the bright spots that aren't tumours?
-# Maybe this is just a bad image and the real images won't have any big bright spots other than the tumour.
-# Then we can do opening and dilation and all that's gonna be left is the tumour.
-# Good approach from here might be to remove everything that's ~200 brightness or lower...
-# Then open, then dilate, then label
-# At this point we should probably be able to segment a few things.
-
-
-
-
-
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-# sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
-# Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
-
-# Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
-
-markers = cv2.watershed(img,markers)
-img[markers == -1] = [255,0,0]
